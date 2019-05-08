19720f2ef5515535d65fb401835c9b0f04f2b9c1
jpagnan0 <a.jpagnano@gmail.com>
2019-05-08 11:15:17 -0400

    netlify

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Landing from "../containers/Landing";
 import NavigationBar from "../containers/NavigationBar";
 import DashBoardContainer from "../containers/DashBoardContainer"
