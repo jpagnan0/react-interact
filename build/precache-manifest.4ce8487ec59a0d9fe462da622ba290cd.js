6eca6b4f627042b7f0588f83b6ddeab1a7b81d1c
jpagnan0 <a.jpagnano@gmail.com>
2019-05-08 11:27:40 -0400

    netlify

---
+++ build/precache-manifest.4ce8487ec59a0d9fe462da622ba290cd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "edf4ad89cd199c428bd3",
+    "url": "/static/js/main.edf4ad89.chunk.js"
+  },
+  {
+    "revision": "c577b7977f6382e9dbaa",
+    "url": "/static/js/2.c577b797.chunk.js"
+  },
+  {
+    "revision": "65e2191e421fd4368b77e24beb5ebd82",
+    "url": "/index.html"
+  }
+];
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Landing from "../containers/Landing";
 import NavigationBar from "../containers/NavigationBar";
-import DashBoardContainer from "../containers/DashBoardContainer"
+import DashBoardContainer from "../containers/DashBoardContainer";
 import SignUp from "../components/SignUp";
 import Login from "../components/Login";
 
 const routes = (
   <div>
-    <NavigationBar />
+    <NavigationBar/>
     <Switch>
-      <Route exact path="/" component={Landing} />
-      <Route path="/dashboard" component={DashBoardContainer} />
-      <Route path="/signup" component={SignUp} />
-      <Route path="/login" component={Login} />
-
+      <Route exact="exact" path="/" component={Landing}/>
+      <Route path="/dashboard" component={DashBoardContainer}/>
+      <Route path="/signup" component={SignUp}/>
+      <Route path="/login" component={Login}/>
     </Switch>
   </div>
 )
